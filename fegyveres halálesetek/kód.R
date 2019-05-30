b3fef15788feb55a97cfcfbdfc1ec4c1b96cc40a
bedobotond <47646580+bedobotond@users.noreply.github.com>
2019-05-30 23:31:26 +0200

    Update kód.R

--- fegyveres halálesetek/kód.R
+++ fegyveres halálesetek/kód.R
@@ -1,147 +1,150 @@
-#ÈvenkÈnti feloszt·s
+#Ez a f√°jl a megold√°sba beker√ºlt √°br√°kat √©s hipot√©zisvizsg√°lati pr√≥b√°lat tartalmazza
+
+
+#√©venk√©nti feloszt√°s
 pie(table(guns$year/97218),radius = 1, labels= NA,col = c("#003300","#006600","#33CC33"),
-    main= "Fegyveres hal·lesetek ÈvenkÈnti bont·sa")
+    main= "Fegyveres hal√°lesetek √©venk√©nti bont√°sa")
 legend(x=1.3,y=1,legend = c(2012,2013,2014),fill =c("#003300","#006600","#33CC33"))
 box()
 
-#hÛnaponkÈnti feloszt·s
+#h√≥naponk√©nti feloszt√°s
 barplot(table(guns$year*guns$month/97218),ylim=c(0,3500),names.arg = NA,las=1,
-        main="Fegyveres hal·lesetek sz·ma havi bont·sban", col = c("#003300","#33CC33"))
+        main="Fegyveres hal√°lesetek sz√°ma havi bont√°sban", col = c("#003300","#33CC33"))
 abline(h=mean(table(guns$year*guns$month/97218)), col=2, lwd=2)
 mtext(text ="2012.01",side=1,cex = 0.6,adj = 0)
 mtext(text ="2014.12",side=1,cex = 0.6,adj = 1)
-text(x=0,y=2850,adj=0,label="¡tlag",col=2,cex = 0.7)
+text(x=0,y=2850,adj=0,label="√Åtlag",col=2,cex = 0.7)
 box()
 
-#nemenkÈnti feloszt·s
-barplot(table(guns$sex)/97218,main = "Fegyveres hal·lesetek nemek kˆzt felosztva",
-        xlab = "Nem",ylab = "%",ylim= c(0,1),las=1,names.arg = c("Nık","FÈrfiak"),col=c("red","blue"))
+#nemenk√©nti feloszt√°s
+barplot(table(guns$sex)/97218,main = "Fegyveres hal√°lesetek nemek k√∂zt felosztva",
+        xlab = "Nem",ylab = "%",ylim= c(0,1),las=1,names.arg = c("N√µk","F√©rfiak"),col=c("red","blue"))
 text(x=0.75,y=0.2,label = 0.1441297)
 text(x=1.9,y=0.9,label = 0.8558703)
 box()
 
-#cÈl alapj·n feloszt·s
-barplot(table(guns$intent)/97218,main = "Fegyverhaszn·lat cÈlja",xlab = "Az ok",ylab = "%",
-        ylim = c(0,1), names.arg = c("Baleset","Gyilkoss·g","÷ngyilkoss·g"),col = c("#003300","#006600","#33CC33"))
+#c√©l alapj√°n feloszt√°s
+barplot(table(guns$intent)/97218,main = "Fegyverhaszn√°lat c√©lja",xlab = "Az ok",ylab = "%",
+        ylim = c(0,1), names.arg = c("Baleset","Gyilkoss√°g","√ñngyilkoss√°g"),col = c("#003300","#006600","#33CC33"))
 text(x=0.7,y=0.1,label = 0.01643729)
 text(x=1.9,y=0.4,label = 0.34282746)
 text(x=3.1,y=0.7,label = 0.64073525)
 box()
 
-#m˚veltsÈg
+#m√ªvelts√©g
 barplot(table(guns$education)/97218,las = 1,
-        main = "Az ·ldozatok m˚veltsÈgi szintjeinek ar·nya",ylab = "%",ylim = c(0,1),col = c("#003300","#006600","#33CC33","#00FF00"))
-legend(x=0.5,y=1,legend = c("…rettsÈgi nÈlk¸li","…rettsÈgivel rendelkezı","Egyetemi kÈpzÈsben rÈszes¸lt","Egyetemi vÈgzettsÈggel rendelkezik"),fill =c("#003300","#006600","#33CC33","#00FF00"))
+        main = "Az √°ldozatok m√ªvelts√©gi szintjeinek ar√°nya",ylab = "%",ylim = c(0,1),col = c("#003300","#006600","#33CC33","#00FF00"))
+legend(x=0.5,y=1,legend = c("√âretts√©gi n√©lk√ºli","√âretts√©givel rendelkez√µ","Egyetemi k√©pz√©sben r√©szes√ºlt","Egyetemi v√©gzetts√©ggel rendelkezik"),fill =c("#003300","#006600","#33CC33","#00FF00"))
 box()
 
 #rassz
-barplot((table(guns$race)/97218)[order((table(guns$race)/97218),decreasing = T)],las = 1, main = "Az adatok rassz alapj·n vizsg·lva",ylab = "%",ylim = c(0,1),
+barplot((table(guns$race)/97218)[order((table(guns$race)/97218),decreasing = T)],las = 1, main = "Az adatok rassz alapj√°n vizsg√°lva",ylab = "%",ylim = c(0,1),
         col = c("#003300","#006600","#33CC33"),xaxt = "n")
 text(x=1.9,y=0.28,label = 0.231983789,cex = 0.7)
 text(x=0.7,y=0.7,label = 0.231983789,cex = 0.7)
 text(x=3.1,y=0.13,label = 0.231983789,cex = 0.7)
-axis(1, at = c(0.75,2,3.1,4.25,5.5), labels = c("FehÈr","Fekete", "Hispan","¡zsiai","’slakos") )
-mtext(text ="Fekete, fehÈr Ès hispan ·ldozatok ˆsszesen: 98%",side=3,cex = 0.75)
+axis(1, at = c(0.75,2,3.1,4.25,5.5), labels = c("Feh√©r","Fekete", "Hispan","√Åzsiai","√ïslakos") )
+mtext(text ="Fekete, feh√©r √©s hispan √°ldozatok √∂sszesen: 98%",side=3,cex = 0.75)
 box()
 
-#helyszÌn
-barplot(table(guns$place)/97218,horiz = T,las=1,xlim = c(0,1),xlab = "%",cex.axis = 0.7,main = "Hal·lesetek helyszÌnÈnek ar·nya",
+#helysz√≠n
+barplot(table(guns$place)/97218,horiz = T,las=1,xlim = c(0,1),xlab = "%",cex.axis = 0.7,main = "Hal√°lesetek helysz√≠n√©nek ar√°nya",
         col = c("#003300","#33CC33"),cex.names = 0.3)
-mtext(text ="Otthon tˆrtÈnt hal·lesetek: 60%",side=3,cex = 0.75)
+mtext(text ="Otthon t√∂rt√©nt hal√°lesetek: 60%",side=3,cex = 0.75)
 box()
 
-#egy histogram arrÛl, hogy a kooral hogyan v·ltozik a fegyveres gyilkoss·gok gyakoris·ga
-hist(guns$age,freq = T,ylim = c(0,15000),breaks = 19,main = "Fegyveres hal·lesetek kor alapj·n",
-     xlab = "Kor",ylab="Elıfordul·s",las=1,cex.axis=0.8)
+#egy histogram arr√≥l, hogy a kooral hogyan v√°ltozik a fegyveres gyilkoss√°gok gyakoris√°ga
+hist(guns$age,freq = T,ylim = c(0,15000),breaks = 19,main = "Fegyveres hal√°lesetek kor alapj√°n",
+     xlab = "Kor",ylab="El√µfordul√°s",las=1,cex.axis=0.8)
 box()
 
-#Most ugyanez, csak sz·ndÈk szerint kÈt rÈszre bontva
-#÷ngyilkoss·g
-hist(guns$age[guns$intent=="Suicide"],freq = T,ylim = c(0,15000),breaks = 19,main = "÷ngyilkoss·g",xlab = "Kor",ylab="Elıfordul·s",las=1,
+#Most ugyanez, csak sz√°nd√©k szerint k√©t r√©szre bontva
+#√ñngyilkoss√°g
+hist(guns$age[guns$intent=="Suicide"],freq = T,ylim = c(0,15000),breaks = 19,main = "√ñngyilkoss√°g",xlab = "Kor",ylab="El√µfordul√°s",las=1,
      cex.axis = 0.8, cex.lab=0.8)
 box()
 
-#a m·sik ·bra
-hist(guns$age[guns$intent=="Homicide"],freq = T,ylim = c(0,15000),breaks = 19,main = "Gyilkoss·g",xlab = "Kor",ylab="Elıfordul·s",las=1,
+#a m√°sik √°bra
+hist(guns$age[guns$intent=="Homicide"],freq = T,ylim = c(0,15000),breaks = 19,main = "Gyilkoss√°g",xlab = "Kor",ylab="El√µfordul√°s",las=1,
      cex.axis = 0.8, cex.lab=0.8)
 box()
 
-#boxploton ·br·zolva
-boxplot(guns$age,main="Hal·lesetek kor alapj·n",xlab="Kor",las=1,ylim=c(0,110),horizontal = T)
+#boxploton √°br√°zolva
+boxplot(guns$age,main="Hal√°lesetek kor alapj√°n",xlab="Kor",las=1,ylim=c(0,110),horizontal = T)
 mtext(text ="Q1 = 27; Q2 = 42; Q3 = 58",side=3,cex = 0.75)
 quantile(guns$age,probs = c(0, 0.25, 0.5, 0.75, 1))
 
-#boxplot nemek kˆzt tˆrve, tanults·g alapj·n plotolva, kor alapj·n sz·molva
-boxplot(guns$age ~ guns$sex * guns$education,main="Kor vs M˚veltsÈg nemek szerint bontva",col= c(2,4),las =1,
-        axes = F,ylab="Kor",xlab="M˚veltsÈg")
+#boxplot nemek k√∂zt t√∂rve, tanults√°g alapj√°n plotolva, kor alapj√°n sz√°molva
+boxplot(guns$age ~ guns$sex * guns$education,main="Kor vs M√ªvelts√©g nemek szerint bontva",col= c(2,4),las =1,
+        axes = F,ylab="Kor",xlab="M√ªvelts√©g")
 axis(2,at=seq(0,120,10),seq(0,120,10),las=1)
 axis(1,at=c(1.5,3.5,5.5,7.5),labels=c(1,2,3,4),las = 1,cex.axis = 0.6)
-legend(x=5.5,y=15.5,legend=c("Nı","FÈrfi"),fill = c(2,4),cex=0.6,horiz=T)
+legend(x=5.5,y=15.5,legend=c("N√µ","F√©rfi"),fill = c(2,4),cex=0.6,horiz=T)
 box()
 
 #Q-Q plot kor
-hist(guns$age,freq = F,ylim = c(0,0.03),breaks = 19,main = "Kor s˚r˚sÈg",
-     xlab = "Kor",ylab="S˚r˚sÈg",las=1,cex.axis=0.8)
+hist(guns$age,freq = F,ylim = c(0,0.03),breaks = 19,main = "Kor s√ªr√ªs√©g",
+     xlab = "Kor",ylab="S√ªr√ªs√©g",las=1,cex.axis=0.8)
 lines(density(guns$age), col = 2)
 box()
 
-qqnorm(guns$age,xlab="ElmÈleti kvantilisek",ylab = "Minta kvantilisek",main="Kor Q-Q Plot",las = 1)
+qqnorm(guns$age,xlab="Elm√©leti kvantilisek",ylab = "Minta kvantilisek",main="Kor Q-Q Plot",las = 1)
 qqline(guns$age,col=2)
 
 
 #adatok felosztva
-hist(guns$age[guns$sex == "M" & guns$race== "White" & guns$intent == "Homicide"],freq = F,main = "FehÈr fÈrfiak gyilkoss·gban haltak meg",xlab = "Kor",las=1,ylim = c(0,0.03),breaks = 19,cex.axis=0.8)
+hist(guns$age[guns$sex == "M" & guns$race== "White" & guns$intent == "Homicide"],freq = F,main = "Feh√©r f√©rfiak gyilkoss√°gban haltak meg",xlab = "Kor",las=1,ylim = c(0,0.03),breaks = 19,cex.axis=0.8)
 lines(density(guns$age[guns$sex == "M" & guns$race== "White" & guns$intent == "Homicide"]),col=2)
 box()
-qqnorm(guns$age[guns$sex == "M" & guns$race== "White" & guns$intent == "Homicide"],xlab="ElmÈleti kvantilisek",ylab = "Minta kvantilisek")
+qqnorm(guns$age[guns$sex == "M" & guns$race== "White" & guns$intent == "Homicide"],xlab="Elm√©leti kvantilisek",ylab = "Minta kvantilisek")
 qqline(guns$age[guns$sex == "M" & guns$race== "White" & guns$intent == "Homicide"],col=2)
 
-hist(guns$age[guns$sex == "F" & guns$race== "White" & guns$intent == "Homicide"],freq = F,main = "FehÈr nık gyilkoss·gban haltak meg",xlab = "Kor",las=1,ylim = c(0,0.03),breaks = 19,cex.axis=0.8)
+hist(guns$age[guns$sex == "F" & guns$race== "White" & guns$intent == "Homicide"],freq = F,main = "Feh√©r n√µk gyilkoss√°gban haltak meg",xlab = "Kor",las=1,ylim = c(0,0.03),breaks = 19,cex.axis=0.8)
 lines(density(guns$age[guns$sex == "F" & guns$race== "White" & guns$intent == "Homicide"]),col=2)
 box()
-qqnorm(guns$age[guns$sex == "F" & guns$race== "White" & guns$intent == "Homicide"],xlab="ElmÈleti kvantilisek",ylab = "Minta kvantilisek")
+qqnorm(guns$age[guns$sex == "F" & guns$race== "White" & guns$intent == "Homicide"],xlab="Elm√©leti kvantilisek",ylab = "Minta kvantilisek")
 qqline(guns$age[guns$sex == "F" & guns$race== "White" & guns$intent == "Homicide"],col=2)
 
-hist(guns$age[guns$sex == "M" & guns$race== "Black" & guns$intent == "Homicide"],freq = F,main = "Fekete fÈrfiak gyilkoss·gban haltak meg",xlab = "Kor",las=1,ylim = c(0,0.06),breaks = 19,cex.axis=0.8)
+hist(guns$age[guns$sex == "M" & guns$race== "Black" & guns$intent == "Homicide"],freq = F,main = "Fekete f√©rfiak gyilkoss√°gban haltak meg",xlab = "Kor",las=1,ylim = c(0,0.06),breaks = 19,cex.axis=0.8)
 lines(density(guns$age[guns$sex == "M" & guns$race== "Black" & guns$intent == "Homicide"]),col=2)
 box()
-qqnorm(guns$age[guns$sex == "M" & guns$race== "Black" & guns$intent == "Homicide"],xlab="ElmÈleti kvantilisek",ylab = "Minta kvantilisek")
+qqnorm(guns$age[guns$sex == "M" & guns$race== "Black" & guns$intent == "Homicide"],xlab="Elm√©leti kvantilisek",ylab = "Minta kvantilisek")
 qqline(guns$age[guns$sex == "M" & guns$race== "Black" & guns$intent == "Homicide"],col=2)
 
-hist(guns$age[guns$sex == "F" & guns$race== "Black" & guns$intent == "Homicide"],freq = F,main = "Fekete nık gyilkoss·gban haltak meg",xlab = "Kor",las=1,ylim = c(0,0.06),breaks = 19,cex.axis=0.8)
+hist(guns$age[guns$sex == "F" & guns$race== "Black" & guns$intent == "Homicide"],freq = F,main = "Fekete n√µk gyilkoss√°gban haltak meg",xlab = "Kor",las=1,ylim = c(0,0.06),breaks = 19,cex.axis=0.8)
 lines(density(guns$age[guns$sex == "F" & guns$race== "Black" & guns$intent == "Homicide"]),col=2)
 box()
-qqnorm(guns$age[guns$sex == "F" & guns$race== "Black" & guns$intent == "Homicide"],xlab="ElmÈleti kvantilisek",ylab = "Minta kvantilisek")
+qqnorm(guns$age[guns$sex == "F" & guns$race== "Black" & guns$intent == "Homicide"],xlab="Elm√©leti kvantilisek",ylab = "Minta kvantilisek")
 qqline(guns$age[guns$sex == "F" & guns$race== "Black" & guns$intent == "Homicide"],col=2)
 
 
-hist(guns$age[guns$sex == "M" & guns$race== "White" & guns$intent == "Suicide"],freq = F,main = "FehÈr fÈrfiak ˆngyilkoss·gban haltak meg",xlab = "Kor",las=1,ylim = c(0,0.03),breaks = 19,cex.axis=0.8)
+hist(guns$age[guns$sex == "M" & guns$race== "White" & guns$intent == "Suicide"],freq = F,main = "Feh√©r f√©rfiak √∂ngyilkoss√°gban haltak meg",xlab = "Kor",las=1,ylim = c(0,0.03),breaks = 19,cex.axis=0.8)
 lines(density(guns$age[guns$sex == "M" & guns$race== "White" & guns$intent == "Suicide"]),col=2)
 box()
-qqnorm(guns$age[guns$sex == "M" & guns$race== "White" & guns$intent == "Suicide"],xlab="ElmÈleti kvantilisek",ylab = "Minta kvantilisek")
+qqnorm(guns$age[guns$sex == "M" & guns$race== "White" & guns$intent == "Suicide"],xlab="Elm√©leti kvantilisek",ylab = "Minta kvantilisek")
 qqline(guns$age[guns$sex == "M" & guns$race== "White" & guns$intent == "Suicide"],col=2)
 
-hist(guns$age[guns$sex == "F" & guns$race== "White" & guns$intent == "Suicide"],freq = F,main = "FehÈr nık ˆngyilkoss·gban haltak meg",xlab = "Kor",las=1,ylim = c(0,0.03),breaks = 19,cex.axis=0.8)
+hist(guns$age[guns$sex == "F" & guns$race== "White" & guns$intent == "Suicide"],freq = F,main = "Feh√©r n√µk √∂ngyilkoss√°gban haltak meg",xlab = "Kor",las=1,ylim = c(0,0.03),breaks = 19,cex.axis=0.8)
 lines(density(guns$age[guns$sex == "F" & guns$race== "White" & guns$intent == "Suicide"]),col=2)
 box()
-qqnorm(guns$age[guns$sex == "F" & guns$race== "White" & guns$intent == "Suicide"],xlab="ElmÈleti kvantilisek",ylab = "Minta kvantilisek")
+qqnorm(guns$age[guns$sex == "F" & guns$race== "White" & guns$intent == "Suicide"],xlab="Elm√©leti kvantilisek",ylab = "Minta kvantilisek")
 qqline(guns$age[guns$sex == "F" & guns$race== "White" & guns$intent == "Suicide"],col=2)
 
-hist(guns$age[guns$sex == "M" & guns$race== "Black" & guns$intent == "Suicide"],freq = F,main = "Fekete fÈrfiak ˆngyilkoss·gban haltak meg",xlab = "Kor",las=1,ylim = c(0,0.06),breaks = 19,cex.axis=0.8)
+hist(guns$age[guns$sex == "M" & guns$race== "Black" & guns$intent == "Suicide"],freq = F,main = "Fekete f√©rfiak √∂ngyilkoss√°gban haltak meg",xlab = "Kor",las=1,ylim = c(0,0.06),breaks = 19,cex.axis=0.8)
 lines(density(guns$age[guns$sex == "M" & guns$race== "Black" & guns$intent == "Suicide"]),col=2)
 box()
-qqnorm(guns$age[guns$sex == "M" & guns$race== "Black" & guns$intent == "Suicide"],xlab="ElmÈleti kvantilisek",ylab = "Minta kvantilisek")
+qqnorm(guns$age[guns$sex == "M" & guns$race== "Black" & guns$intent == "Suicide"],xlab="Elm√©leti kvantilisek",ylab = "Minta kvantilisek")
 qqline(guns$age[guns$sex == "M" & guns$race== "Black" & guns$intent == "Suicide"],col=2)
 
-hist(guns$age[guns$sex == "F" & guns$race== "Black" & guns$intent == "Suicide"],freq = F,main = "Fekete nık ˆngyilkoss·gban haltak meg",xlab = "Kor",las=1,ylim = c(0,0.03),breaks = 19,cex.axis=0.8)
+hist(guns$age[guns$sex == "F" & guns$race== "Black" & guns$intent == "Suicide"],freq = F,main = "Fekete n√µk √∂ngyilkoss√°gban haltak meg",xlab = "Kor",las=1,ylim = c(0,0.03),breaks = 19,cex.axis=0.8)
 lines(density(guns$age[guns$sex == "F" & guns$race== "Black" & guns$intent == "Suicide"]),col=2)
 box()
-qqnorm(guns$age[guns$sex == "F" & guns$race== "Black" & guns$intent == "Suicide"],xlab="ElmÈleti kvantilisek",ylab = "Minta kvantilisek")
+qqnorm(guns$age[guns$sex == "F" & guns$race== "Black" & guns$intent == "Suicide"],xlab="Elm√©leti kvantilisek",ylab = "Minta kvantilisek")
 qqline(guns$age[guns$sex == "F" & guns$race== "Black" & guns$intent == "Suicide"],col=2)
 
 
-#HipotÈzisvizsg·lat
-#egymint·s t-prÛba
+#Hipot√©zisvizsg√°lat
+#egymint√°s t-pr√≥ba
 t.test(guns$age[guns$sex == "M" & guns$race== "White" & guns$intent == "Homicide"],mu=32.4,alternative = "greater")
 
 t.test(guns$age[guns$sex == "F" & guns$race== "White" & guns$intent == "Homicide"],mu=39.3,alternative = "greater")
@@ -158,16 +161,16 @@ t.test(guns$age[guns$sex == "M" & guns$race== "Black" & guns$intent == "Suicide"
 
 t.test(guns$age[guns$sex == "F" & guns$race== "Black" & guns$intent == "Suicide"],mu=38.4,alternative = "greater")
 
-#kÈtmint·s t-prÛba
+#k√©tmint√°s t-pr√≥ba
 t.test(guns$age[guns$sex== "F"],guns$age[guns$sex== "M"],alternative="greater")
 
-#khÌ-nÈgyzet prÛba
-barplot(table(guns$intent,guns$sex),beside = T,ylim=c(0,60000),ylab="Gyilkoss·gok sz·ma",las=2, main= "Nem Ès sz·ndÈk", names.arg = c("Nı","FÈrfi"),las=1,col = c("#003300","#006600","#33CC33"),cex.axis = 0.8) 
-legend(x=1,y=50000,legend = c("Baleset","Gyilkoss·g","÷ngyilkoss·g"),fill= c("#003300","#006600","#33CC33"))
+#kh√≠-n√©gyzet pr√≥ba
+barplot(table(guns$intent,guns$sex),beside = T,ylim=c(0,60000),ylab="Gyilkoss√°gok sz√°ma",las=2, main= "Nem √©s sz√°nd√©k", names.arg = c("N√µ","F√©rfi"),las=1,col = c("#003300","#006600","#33CC33"),cex.axis = 0.8) 
+legend(x=1,y=50000,legend = c("Baleset","Gyilkoss√°g","√ñngyilkoss√°g"),fill= c("#003300","#006600","#33CC33"))
 box()
 chisq.test(table(guns$intent,guns$sex), correct = T)
 
-barplot(table(guns$year,guns$sex),beside = T,ylim=c(0,60000),ylab="Gyilkoss·gok sz·ma",las=2, main= "Nem Ès Èv", names.arg = c("Nı","FÈrfi"),las=1,col = c("#003300","#006600","#33CC33"),cex.axis = 0.8) 
+barplot(table(guns$year,guns$sex),beside = T,ylim=c(0,60000),ylab="Gyilkoss√°gok sz√°ma",las=2, main= "Nem √©s √©v", names.arg = c("N√µ","F√©rfi"),las=1,col = c("#003300","#006600","#33CC33"),cex.axis = 0.8) 
 legend(x=1,y=50000,legend = c(2012,2013,2014),fill= c("#003300","#006600","#33CC33"))
 box()
 chisq.test(table(guns$year,guns$sex), correct = T)
